86f83acf836a1e745f707c1046cbfd9e2aae5280
shibo <qus4@miamioh.edu>
2025-12-13 15:32:30 -0500

    Update data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -3,20 +3,35 @@ import os
 
 DATABASE = '/nfs/demo.db'
 
-def connect_db():
-    """Connect to the SQLite database."""
-    return sqlite3.connect(DATABASE)
+def get_db():
+    db = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+def init_table():
+    db = get_db()
+    db.execute('''
+        CREATE TABLE IF NOT EXISTS contacts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            phone TEXT NOT NULL
+        );
+    ''')
+    db.commit()
+    db.close()
+    print("[INIT] contacts table ensured.")
 
 def generate_test_data(num_contacts):
-    """Generate test data for the contacts table."""
-    db = connect_db()
+    db = get_db()
     for i in range(num_contacts):
         name = f'Test Name {i}'
         phone = f'123-456-789{i}'
         db.execute('INSERT INTO contacts (name, phone) VALUES (?, ?)', (name, phone))
     db.commit()
-    print(f'{num_contacts} test contacts added to the database.')
+    print(f"[OK] {num_contacts} test contacts added.")
     db.close()
 
 if __name__ == '__main__':
-    generate_test_data(10)  # Generate 10 test contacts.
+    init_table()
+    generate_test_data(10)
+
